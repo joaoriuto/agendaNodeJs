e81165efc752ee3ceb83e3b5ebb1ba89eaa7e6b9
João Riuto <jr00.dat@gmail.com>
2020-11-28 01:55:46 -0300

    Organização de responsabilidades e conf do servidor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express()
+const customExpress = require('./config/customExpress')
+const app = customExpress()
+
 
 app.listen(3000, () => console.log('Servidor Rodando na porta 3000'))
-app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos, tudo 200!!'))
