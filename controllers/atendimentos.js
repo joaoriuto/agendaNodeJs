c75abd01176b137345131c43d765660e7537e2dd
João Riuto <jr00.dat@gmail.com>
2020-11-29 14:58:48 -0300

    Modulos de acesso ao BD

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,12 @@
-module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos.'))
+const Atendimento = require('../models/atendimentos')
 
-}
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos GET.'))
+
+    app.post('/atendimentos', (req, res) => { 
+        const atendimento = req.body
+        
+        Atendimento.adiciona(atendimento)
+        res.send('Rota de antendimentos realizando um POST')
+    })
+} 
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const customExpress = require('./config/customExpress')
-const app = customExpress()
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
+conexao.connect(erro => {
+    if (erro) {
+        console.log("---> " + erro)
+    } else {
+        console.log('Conectado com sucesso')
 
-app.listen(3000, () => console.log('Servidor Rodando na porta 3000'))
+        Tabelas.init(conexao)
+        const app = customExpress()
+        app.listen(3000, () => console.log('Servidor Rodando na porta 3000'))
+
+    }
+})
+
