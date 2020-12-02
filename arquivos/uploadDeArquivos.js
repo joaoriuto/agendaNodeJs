f45dc406c1058074e6e4f7f71409b685d5825a74
João Riuto <jr00.dat@gmail.com>
2020-12-02 10:27:57 -0300

    Remoção do buffer e adição do stream de arquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,5 @@
 const fs = require('fs')
 
-fs.readFile('./assets/floc.jpg', (erro, buffer) => {
-    console.log('Imagem foi bufferizada')
-    
-    fs.writeFile('./assets/flocCopy.jpg', buffer, erro => {
-        console.log('imagem foi escrita')
-    })
-})
+fs.createReadStream('./assets/floc.jpg')
+    .pipe(fs.createWriteStream('./assets/floc-stream.jpg'))
+    .on('finish', () => console.log('Imagem foi escrita'))
